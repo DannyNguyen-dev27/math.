264a3d617add3b634361e1f9a0175727cc3cbdda
DannyNguyen <thaiduongnguyenngoc27@gmail.com>
2025-07-03 18:51:38 +0700

    Add files via upload

---
+++ 123 (2).py
@@ -0,0 +1,7 @@
+#thêm 2 biến chiều dài và chiều rộng
+length = int(input("Nhập chiều dài cạnh: "))
+width = int(input("Nhập chiểu rộng hình chữ nhật: "))
+total = length * width #biến total bằng tích của 2 biến chiều dài và chiều rộng
+print("Diện tích hình chữ nhật là:")
+print(str(length) + " * " + str(width) + " = " + str(total))
+print("Diện tích hình chữ nhật là " + str(total)) 
