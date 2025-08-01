de18f1220d1741588b7a570aac56c7ae8c78c67b
DannyNguyen <thaiduongnguyenngoc27@gmail.com>
2025-08-01 22:32:04 +0700

    Add files via upload

---
+++ Bài_28(undone).py
@@ -0,0 +1,48 @@
+'''
+Bài tập:
+xây dựng 1 từ điển có các chức năng như sau(người dùng nhập chức năng muốn dùng qua menu)
+1.Thêm 1 từ vừng mới(kèm nghĩa của từ đó)
+2.tra cứu nghĩa của 1 từ vựng
+3.cập nhật ý nghĩa của 1 từ vựng
+4.cho phép người dùng xóa đi từ vựng trong từ điển
+5.cho phép người dùng xóa toàn bộ từ vựng
+6.cho phép người dùng in ra toàn bộ từ vựng
+7.cho phép người dùng in ra từ điển theo cấu trúc: "TỪ VỰNG"
+8.kết thúc chương trình
+'''
+#bài giải
+tudien = {} #khai báo từ điển
+
+while(True){  
+    print("Vui lòng lựa chọn chức năng(số): ")
+    print('''
+    1.Thêm 1 từ vừng mới(kèm nghĩa của từ đó)\n  
+    2.tra cứu nghĩa của 1 từ vựng\n
+    3.cập nhật ý nghĩa của 1 từ vựng\n
+    4.cho phép người dùng xóa đi từ vựng trong từ điển\n
+    5.cho phép người dùng xóa toàn bộ từ vựng\n
+    6.cho phép người dùng in ra toàn bộ từ vựng\n
+    7.cho phép người dùng in ra từ điển theo cấu trúc: "TỪ VỰNG"\n
+    8.kết thúc chương trình
+    ''')
+    luachon = int(input("Nhập lựa chọn: "))
+    
+    if (luachon == 1):
+
+    elif (luachon == 2):
+
+    elif (luachon == 3):
+    
+    elif (luachon == 4):
+    
+    elif (luachon == 5):
+    
+    elif (luachon == 6):
+    
+    elif (luachon == 7):
+    
+    elif (luachon == 8):
+    
+    else:
+
+}
